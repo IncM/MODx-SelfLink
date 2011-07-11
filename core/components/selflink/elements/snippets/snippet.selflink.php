65387a17e14dd24e77c97b92347acde4d3a1de35
Bert Oost <bert@server.oostdesign.nl>
2011-07-11 20:19:08 +0200

    Fixed issues with non chunk using of selflink

--- core/components/selflink/elements/snippets/snippet.selflink.php
+++ core/components/selflink/elements/snippets/snippet.selflink.php
@@ -24,66 +24,65 @@ $tpl = $modx->getOption('tpl', $scriptProperties, false);
 $linktxt = $modx->getOption('linktext', $scriptProperties, false);
 
 if($res = $modx->getObject('modResource', $resource)) {
-    
+	
 	$menuindex = $res->get('menuindex');
-    $parentid = $res->get('parent');
-    $c = $modx->newQuery('modResource');
-    $c->limit(1);
+	$parentid = $res->get('parent');
+	$c = $modx->newQuery('modResource');
+	$c->limit(1);
 
-    switch($direction) {
+	switch($direction) {
 
-        case 'up':
-        case 'parent':
-            $c->where(array('id' => $parentid));
+		case 'up':
+		case 'parent':
+			$c->where(array('id' => $parentid));
 		break;
 
-        case 'next':
-        case 'prev':
-        case 'previous':
-            $c->where(array('id:!=' => $resource));
-            $c->where(array('parent' => $parentid));
-            $c->where(array('published' => true));
-            $c->where(array('deleted' => false));
+		case 'next':
+		case 'prev':
+		case 'previous':
+			$c->where(array('id:!=' => $resource));
+			$c->where(array('parent' => $parentid));
+			$c->where(array('published' => true));
+			$c->where(array('deleted' => false));
 		break;
-    }
+	}
 	
-    switch($direction) {
+	switch($direction) {
 
-        case 'next':
-            $c->where("IF(menuindex = ". $menuindex .",id > ". $resource .",menuindex >". $menuindex . ")" , xPDOQuery::SQL_AND);		
-            $c->sortby('menuindex,id','ASC');
+		case 'next':
+			$c->where("IF(menuindex = ". $menuindex .",id > ". $resource .",menuindex >". $menuindex . ")" , xPDOQuery::SQL_AND);		
+			$c->sortby('menuindex,id','ASC');
 		break;
 
-        case 'prev':
-        case 'previous':
-            $c->where("IF(menuindex = ". $menuindex .",id < ". $resource .",menuindex <". $menuindex . ")" , xPDOQuery::SQL_AND);		
-            $c->sortby('menuindex','DESC');
-            $c->sortby('id','DESC');
+		case 'prev':
+		case 'previous':
+			$c->where("IF(menuindex = ". $menuindex .",id < ". $resource .",menuindex <". $menuindex . ")" , xPDOQuery::SQL_AND);		
+			$c->sortby('menuindex','DESC');
+			$c->sortby('id','DESC');
 		break;
-    }
+	}
 	
-    if($linkResource = $modx->getObject('modResource', $c)) {
-
-        // build placeholders
-        $placeholders = array(
-        	'id' => $linkResource->get('id'),
-        	'pagetitle' => (!empty($linktxt)) ? $linktxt : $linkResource->get('pagetitle'), 
-        	'longtitle' => (!empty($linktxt)) ? $linktxt : $linkResource->get('longtitle'),
-        	'menutitle' => (!empty($linktxt)) ? $linktxt : $linkResource->get('menutitle')
-		);
-
-        // parse chunk
-        $chunk = $modx->getObject('modChunk', array('name' => $tpl));
-
-        if(!$chunk) {
-
-            $useChunk = '<a href="[[~[[+id]]]]">[[+menutitle]]</a>';
-            $chunk = $modx->newObject('modChunk');
-            $chunk->setCacheable(false);
-
-            return $chunk->process($placeholders, $useChunk);
-        }
+	if($linkResource = $modx->getObject('modResource', $c)) {
 		
-        return $chunk->process($placeholders);
-    }
+		// build placeholders
+		$placeholders = array(
+			'id' => $linkResource->get('id'),
+			'pagetitle' => (!empty($linktxt)) ? $linktxt : $linkResource->get('pagetitle'), 
+			'longtitle' => (!empty($linktxt)) ? $linktxt : $linkResource->get('longtitle'),
+			'menutitle' => (!empty($linktxt)) ? $linktxt : $linkResource->get('menutitle')
+		);
+		
+		// parse chunk
+		$chunk = $modx->getObject('modChunk', array('name' => $tpl));
+
+		if(!$chunk) {
+
+			$useChunk = '<a href="[[~[[+id]]]]">[[+menutitle:isempty=`[[+pagetitle]]`]]</a>';
+			$chunk = $modx->newObject('modChunk');
+			$chunk->setCacheable(false);
+			$chunk->setContent($useChunk);
+		}
+		
+		return $chunk->process($placeholders);
+	}
 }
